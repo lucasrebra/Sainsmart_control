6524b4b3135ce07313f06ad9dc6aadbea2e68336
Lucas <lucasrebra97@gmail.com>
2021-02-22 18:48:27 +0100

    las articulaciones se publican de forma desordenada, arreglado en el programa

--- acciona_motor/python/nodo_simple.py
+++ acciona_motor/python/nodo_simple.py
@@ -56,27 +56,28 @@ max_angle4=90"""
 
 #Vector for PWMman , PWMmin, max angles and min angles
 #The servo 5 now isn't working 
-zero_pwm=[280,280,120,280,0,250]
-min_pwm=[100,440,100,100,0,100]
-max_pwm=[500,150,500,500,0,500]
-min_angle=[-90,-81,-5,-90,0,-90]
-max_angle=[90,72,150,90,0,90]
+zero_pwm=[280,300,160,280]#,0,250]
+min_pwm=[100,500,100,100]#,0,100]
+max_pwm=[500,100,500,500]#,0,500]
+min_angle=[-90,-81,-5,-90]#,0,-90]
+max_angle=[90,72,150,90]#,0,90]
+zero_angle=[0,0,0,0]
 
 def callback(jointstate):
     global posicion
     #Calculamos
-    posicion.data[0]=(jointstate.position[0]*radtodegree-min_angle[0])*(max_pwm[0]-min_pwm[0])/(max_angle[0]-min_angle[0])+min_pwm[0]
+    posicion.data[0]=(jointstate.position[0]*radtodegree-zero_angle[0])*(max_pwm[0]-zero_pwm[0])/(max_angle[0]-zero_angle[0])+zero_pwm[0]
 #jointstate.position[0]*radtodegree*(max_pwm1-min_pwm1)/(max_angle1-min_angle1)+min_pwm1
-    posicion.data[1]=(jointstate.position[1]*radtodegree-min_angle[1])*(max_pwm[1]-min_pwm[1])/(max_angle[1]-min_angle[1])+min_pwm[1]
+    posicion.data[1]=(jointstate.position[2]*radtodegree-zero_angle[1])*(max_pwm[1]-zero_pwm[1])/(max_angle[1]-zero_angle[1])+zero_pwm[1]
     #posicion.data[1]=jointstate.position[1]*(-1.80555555)+280
-#jointstate.position[1]*radtodegree*(max_pwm2-min_pwm2)/(max_angle2-min_angle2)+min_pwm2
-    posicion.data[2]=((jointstate.position[2]+jointstate.position[1])*radtodegree-min_angle[2])*(max_pwm[2]-min_pwm[2])/(max_angle[2]-min_angle[2])+min_pwm[2]
-#jointstate.position[2]*radtodegree*(max_pwm3-min_pwm3)/(max_angle3-min_angle3)+min_pwm3
-    posicion.data[3]=(jointstate.position[3]*radtodegree-min_angle[3])*(max_pwm[3]-min_pwm[3])/(max_angle[3]-min_angle[3])+min_pwm[3]
-     
-		posicion.data[4]=(jointstate.position[4]*radtodegree-min_angle[4])*(max_pwm[4]-min_pwm[4])/(max_angle[4]-min_angle[4])+min_pwm[4]
+#jointstate.position[1]*radtodegree*(max_pwm2-zero_pwm2)/(max_angle2-zero_angle2)+zero_pwm2
+    posicion.data[2]=((jointstate.position[3]+jointstate.position[2])*radtodegree-zero_angle[2])*(max_pwm[2]-zero_pwm[2])/(max_angle[2]-zero_angle[2])+zero_pwm[2]
 
-		posicion.data[5]=(jointstate.position[5]*radtodegree-min_angle[5])*(max_pwm[5]-min_pwm[5])/(max_angle[5]-min_angle[5])+min_pwm[5]
+    posicion.data[3]=(jointstate.position[4]*radtodegree-zero_angle[3])*(max_pwm[3]-zero_pwm[3])/(max_angle[3]-zero_angle[3])+zero_pwm[3]
+     
+		#posicion.data[4]=(jointstate.position[4]*radtodegree-min_angle[4])*(max_pwm[4]-min_pwm[4])/(max_angle[4]-min_angle[4])+min_pwm[4]
+
+		#posicion.data[5]=(jointstate.position[5]*radtodegree-min_angle[5])*(max_pwm[5]-min_pwm[5])/(max_angle[5]-min_angle[5])+min_pwm[5]
 #If the angles are between limits OK, if not PWM max or me depending
 #    for n in range(3):
  #       if posicion.data[n]> max_pwm[n]:
